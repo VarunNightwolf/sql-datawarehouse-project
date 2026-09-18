2b18602885c11984da4236f0ff88084be6f8f0a9
VarunNightwolf <mesmeris3@gmail.com>
2026-09-19 03:41:45 +0530

    Update init_silver_tables.sql

--- scripts/silver/init_silver_tables.sql
+++ scripts/silver/init_silver_tables.sql
@@ -3,12 +3,12 @@ create table silver.crm_cust_info (cst_id int, cst_key nvarchar(25),	cst_firstna
 cst_marital_status nvarchar(55),	cst_gndr nvarchar(55),	cst_create_date date, dwh_create_date datetime2 default getdate());
 
 if object_id('silver.crm_prd_info','U') IS NOT NULL DROP TABLE silver.crm_prd_info;
-create table silver.crm_prd_info (prd_id int,prd_key nvarchar(55),	prd_nm nvarchar(55), prd_cost int,	prd_line nvarchar(55),	prd_start_dt datetime,
-prd_end_dt datetime, dwh_create_date datetime2 default getdate());
+create table silver.crm_prd_info (prd_id int,cat_id varchar(55), prd_key nvarchar(55),	prd_nm nvarchar(55), prd_cost int,	prd_line nvarchar(55),	prd_start_dt date,
+prd_end_dt date, dwh_create_date datetime2 default getdate());
 
 if object_id('silver.crm_sales_details','U') IS NOT NULL DROP TABLE silver.crm_sales_details;
-create table silver.crm_sales_details (sls_ord_num nvarchar(55),sls_prd_key	nvarchar(55), sls_cust_id int,	sls_order_dt int,	sls_ship_dt int,
-sls_due_dt int,	sls_sales int,	sls_quantity int,	sls_price int, dwh_create_date datetime2 default getdate());
+create table silver.crm_sales_details (sls_ord_num nvarchar(55),sls_prd_key	nvarchar(55), sls_cust_id int,	sls_order_dt date,	sls_ship_dt date,
+sls_due_dt date,	sls_sales int,	sls_quantity int,	sls_price int, dwh_create_date datetime2 default getdate());
 
 if object_id('silver.erp_cust_az12','U') IS NOT NULL DROP TABLE silver.erp_cust_az12;
 create table silver.erp_cust_az12 (CID nvarchar(55),BDATE date,	GEN nvarchar(55), dwh_create_date datetime2 default getdate());
