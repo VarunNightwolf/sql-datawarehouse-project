143d7672b2cb590afdc0a294563ce35dd54b8a9a
VarunNightwolf <mesmeris3@gmail.com>
2026-09-19 02:38:31 +0530

    Create inserting_into_silver.crm_cust_info.sql
    Cleaning data from bronze.crm_cust_info and adding it into silver.crm_cust_info

---
+++ scripts/silver/inserting_into_silver.crm_cust_info.sql
@@ -0,0 +1,14 @@
+-- Check for Nulls or Duplicates in Primary Key
+-- Check for unwanted spaces
+-- Standardize values for cst_gndr and cst_marital_status
+-- Expectation: No Result
+
+Insert into silver.crm_cust_info (cst_id, cst_key, cst_firstname, cst_lastname,cst_marital_status,cst_gndr,cst_create_date)
+select cst_id, cst_key, trim(cst_firstname), trim(cst_lastname),  
+case when upper(trim(cst_marital_status)) = 'M' then 'Married'
+when upper(trim(cst_marital_status)) = 'S' then 'Single' else 'N/A' end as cst_marital_status,
+case when upper(trim(cst_gndr))='M' then 'Male'
+when upper(trim(cst_gndr)) = 'F' then 'Female' else 'N/A' End as cst_gndr, 
+cst_create_date from(
+select *, rank() over(partition by cst_id order by cst_create_date desc) as flag_last from bronze.crm_cust_info)t where cst_id is not null and flag_last = 1;
+
