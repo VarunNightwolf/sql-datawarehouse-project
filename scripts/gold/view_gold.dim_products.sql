35edf6d9ab4e35727aae30bdd97ea2b06420f658
VarunNightwolf <mesmeris3@gmail.com>
2026-09-19 07:09:00 +0530

    Create view_gold.dim_products.sql
    Creating view for object customers which has all the basic information for each product

---
+++ scripts/gold/view_gold.dim_products.sql
@@ -0,0 +1,19 @@
+create view gold.dim_products as
+
+select 
+row_number() over(order by pn.prd_start_dt, pn.prd_key) as product_key,
+pn.prd_id as product_id, 
+pn.prd_nm as product_name, 
+pn.prd_key as product_number, 
+pn.cat_id as category_id, 
+pc.cat as category, 
+pc.subcat as subcategory,
+pc.maintenance as maintenance,
+pn.prd_cost as product_cost, 
+pn.prd_line as product_line, 
+pn.prd_start_dt as start_date 
+
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is NULL -- filter out all historical data
